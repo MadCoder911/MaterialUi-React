8b5fe22f7763edf1c532cd6d4a60facb11128ae0
Royce119 <madcoder911@gmail.com>
2023-08-29 23:28:38 +0300

    used mui buttons, icons and icon buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 
 import "./App.css";
 import MuiTypography from "./components/MuiTypography";
+import MuiBtn from "./components/MuiBtn";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
       <MuiTypography />
-    </>
+      <br />
+      <hr />
+      <br />
+      <MuiBtn />
+    </div>
   );
 }
 
